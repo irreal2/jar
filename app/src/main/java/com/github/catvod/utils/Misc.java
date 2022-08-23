ca9cb915f15f4e684de0b1965c6c77cb541914d8
harrybot0827 <108169011+harrybot0827@users.noreply.github.com>
2022-08-23 15:15:38 +0800

    Add files via upload

--- app/src/main/java/com/github/catvod/utils/Misc.java
+++ app/src/main/java/com/github/catvod/utils/Misc.java
@@ -39,11 +39,11 @@ public class Misc {
         return false;
     }
 
-    private static final Pattern snifferMatch = Pattern.compile("http((?!http).){26,}?\\.(m3u8|mp4)\\?.*|http((?!http).){26,}\\.(m3u8|mp4)|http((?!http).){26,}?/m3u8\\?pt=m3u8.*|http((?!http).)*?default\\.ixigua\\.com/.*|http((?!http).)*?cdn-tos[^\\?]*|http((?!http).)*?/obj/tos[^\\?]*|http.*?/player/m3u8play\\.php\\?url=.*|http.*?/player/.*?[pP]lay\\.php\\?url=.*|http.*?/playlist/m3u8/\\?vid=.*|http.*?\\.php\\?type=m3u8&.*|http.*?/download.aspx\\?.*|http.*?/api/up_api.php\\?.*|https.*?\\.66yk\\.cn.*|http((?!http).)*?netease\\.com/file/.*");
+    private static final Pattern snifferMatch = Pattern.compile("http((?!http).)*?default\\.365yg\\.com/.*|http((?!http).){26,}?\\.(m3u8|mp4|flv|avi|mkv|mov|3gp|asf|rm|rmvb|wmv|mpg|mpeg|mpe|ts|vob|mp3|wma)\\?.*|http((?!http).){26,}\\.(m3u8|mp4|flv|avi|mkv|mov|3gp|asf|rm|rmvb|wmv|mpg|mpeg|mpe|ts|vob|mp3|wma)|http((?!http).){26,}?/m3u8\\?pt=m3u8.*|http((?!http).)*?default\\.ixigua\\.com/.*|http((?!http).)*?cdn-tos[^\\?]*|http((?!http).)*?/obj/tos[^\\?]*|http.*?/player/m3u8play\\.php\\?url=.*|http.*?/player/.*?[pP]lay\\.php\\?url=.*|http.*?/playlist/m3u8/\\?vid=.*|http.*?\\.php\\?type=m3u8&.*|http.*?/download.aspx\\?.*|http.*?/api/up_api.php\\?.*|http.*?\\.66yk\\.cn.*|http((?!http).)*?netease\\.com/file/.*|http((?!http).)*?douyin\\.com/.*/play/\\?.*|http((?!http).)*?huoshan\\.com/.*/\\?item.*");
 
     public static boolean isVideoFormat(String url) {
         if (snifferMatch.matcher(url).find()) {
-            if (url.contains("cdn-tos") && url.contains(".js")) {
+            if (url.contains(".js") || url.contains(".css") || url.contains(".jpg") || url.contains(".png") || url.contains(".gif") || url.contains(".ico") || url.contains("rl=") || url.contains(".html")) {
                 return false;
             }
             return true;
@@ -101,7 +101,7 @@ public class Misc {
         if (url.startsWith("//")) {
             url = "https:" + url;
         }
-        if (!url.startsWith("http")) {
+        if (!url.startsWith("http") && !url.startsWith("magnet")) {
             return null;
         }
         if (url.equals(input)) {
