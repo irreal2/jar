df6a6903d3afa5dca4d5ad3196756254532a067b
harrybot0827 <108169011+harrybot0827@users.noreply.github.com>
2022-08-30 18:49:45 +0800

    Add files via upload

--- app/src/main/java/com/github/catvod/spider/XBiubiu.java
+++ app/src/main/java/com/github/catvod/spider/XBiubiu.java
@@ -360,7 +360,7 @@ public class XBiubiu extends Spider {
             vod.put("vod_content", desc);
 	       ArrayList<String> playFrom = new ArrayList<>();
            String xlparseContent = html;
-           if ((getRuleVal("xlbiaotiqian").isEmpty() || getRuleVal("xlbiaotihou").isEmpty() || getRuleVal("xlbiaotihou").equals("空")) && !getRuleVal("是否启用播放线路名").equals("1")){
+           if ((getRuleVal("线路名标题后").isEmpty() || getRuleVal("xlbiaotihou").isEmpty() || getRuleVal("线路名标题后").equals("空") || getRuleVal("xlbiaotihou").equals("空")) && !getRuleVal("是否启用播放线路名").equals("1")){
                for (int i = 0; i < playList.size(); i++) {
                    playFrom.add("播放列表" + (i + 1));
                }
@@ -372,7 +372,7 @@ public class XBiubiu extends Spider {
                String xljiequshuzuhou = "xljiequshuzuhou";
                String xlbiaotiqian = "xlbiaotiqian";
                String xlbiaotihou = "xlbiaotihou";
-               if (!getRuleVal("线路名标题后").isEmpty() && !getRuleVal("线路名标题后").equals("空")) {
+               if (!getRuleVal("线路名标题前").isEmpty() && !getRuleVal("线路名标题后").equals("空")) {
                    xlshifouercijiequ = "线路名是否二次截取";
                    xljiequqian = "线路名截取前";
                    xljiequhou = "线路名截取后";
